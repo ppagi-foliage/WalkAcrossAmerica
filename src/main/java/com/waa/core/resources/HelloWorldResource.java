c34f18ef08121a0786b3231ff75ec14e99e756a0
Puneet Pagi <ppagi@foliage.com>
2016-10-13 10:19:53 -0400

    Removed content type

--- src/main/java/com/waa/core/resources/HelloWorldResource.java
+++ src/main/java/com/waa/core/resources/HelloWorldResource.java
@@ -1,14 +1,11 @@
 package com.waa.core.resources;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/hello",
-produces=MediaType.APPLICATION_JSON_VALUE,
-consumes=MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value="/hello")
 public class HelloWorldResource {
 
 	@RequestMapping(method=RequestMethod.GET,path="/")
